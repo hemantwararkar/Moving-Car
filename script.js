a4891ca72905f3eea11dcbf923b269d969d10cdf
hemantwararkar <131163396+hemantwararkar@users.noreply.github.com>
2024-02-01 13:33:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  document.addEventListener("DOMContentLoaded", function() {
       var audio = document.createElement('audio');
-      audio.setAttribute('src', './sound.mp3'); 
+      audio.setAttribute('src', 'sound.mp3'); 
       audio.loop = true;
       document.body.appendChild(audio);
       audio.play();
